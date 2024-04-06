be81386843fe41bc48b39fc172240e6860a4d3fc
Clear008 <wissal-exo@hotmail.com>
2024-04-06 23:30:21 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-"""Distributes an archive to your web servers"""
-from fabric.api import put, run, env
-from datetime import datetime
-import os
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
+from fabric.api import put, run, env
+from os.path import exists
 env.hosts = ['18.206.208.113', '18.206.232.93']
-env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers"""
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
-        put(archive_path, '/tmp/')
-        archive_name = os.path.basename(archive_path)
-        archive_base = os.path.splitext(archive_name)[0]
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-
-        run('mkdir -p {}{}/'.format(path, archive_base))
-        run('tar -xzf /tmp/{} -C {}{}/'
-            .format(archive_name, path, archive_base))
-        run('rm /tmp/{}'.format(archive_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'
-            .format(path, archive_base))
-        run('rm -rf {}{}/web_static'.format(path, archive_base))
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, archive_base))
-        run('chmod -R 755 /data/')
-        print("New version deployed!")
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except FileNotFoundError:
         return False

81511d7e0ee5d4933f8006155b147f4564c752f3
Clear008 <wissal-exo@hotmail.com>
2024-04-07 01:46:57 +0000

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """creates and distributes an archive to your web servers"""
 from fabric.api import put, run, env, local
+from fabric.contrib.files import exists
 from datetime import datetime
 import os
 

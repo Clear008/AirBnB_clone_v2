2cf04426dc827be416843bbd392eea720542c711
Clear008 <wissal-exo@hotmail.com>
2024-04-07 00:04:05 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,18 @@
 # Script that sets up your web servers for the deployment of web_static
 apt-get -y update
 apt-get -y install nginx
-service nginx start
+sudo ufw allow 'Nginx HTTP'
+
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Airbnb clone v2" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 service nginx restart

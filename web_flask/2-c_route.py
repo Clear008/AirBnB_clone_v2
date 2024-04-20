af0d193649d776723c2f48bb7f5e4baab72140d6
Clear008 <wissal-exo@hotmail.com>
2024-04-20 17:42:42 +0100

    0-hello_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,7 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def displayc(text):
+def display_c(text):
     """ display “C ” followed by the value of the text variable"""
     return 'C' + text.replace('_', ' ')
 
